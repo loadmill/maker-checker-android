e9c4610e7dafd894e9e73b989fa8503a453e0d7e
Ido Cohen <ido@loadmill.com>
2025-08-15 20:22:13 +0300

    Hello screen with Compose, SDK config

---
+++ app/src/main/java/com/loadmill/makerchecker/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.loadmill.makerchecker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.loadmill.makerchecker.ui.theme.MakercheckerandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MakercheckerandroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text("Hello, Loadmill", modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MakercheckerandroidTheme {
+        Greeting("Android")
+    }
+}
